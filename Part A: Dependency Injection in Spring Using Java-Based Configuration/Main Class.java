b62d68ee406e3d5fb37a11ef46a87768dfb41d13
pranav211004 <pranav2120041988@gmail.com>
2025-11-06 19:41:18 +0530

    Create Main Class.java

---
+++ Part A: Dependency Injection in Spring Using Java-Based Configuration/Main Class.java
@@ -0,0 +1,11 @@
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        StudentService service = context.getBean(StudentService.class);
+        service.showStudent();
+    }
+}
